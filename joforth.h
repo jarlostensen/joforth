912d71c66a679c02fe94183ba2f96e72d73d89e7
Jarl Ostensen <jarl@polystream.com>
2021-02-08 14:37:19 +0000

    added some documentation.

--- joforth.h
+++ joforth.h
@@ -1,3 +1,11 @@
+#pragma once 
+
+// =======================================================================
+// joForth
+// A very simple, and yet incomplete, Forth interpreter and compiler. 
+// 
+// 
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
@@ -18,6 +26,7 @@ typedef struct _joforth_dict_entry _joforth_dict_entry_t;
 
 typedef void (*joforth_word_handler_t)(joforth_t* joforth);
 
+// used internally
 typedef enum _joforth_word_type {
 
     kWordType_Handler = 1,
@@ -27,7 +36,7 @@ typedef enum _joforth_word_type {
     kWordType_TypeMask = 7
 
 } _joforth_word_type_t;
-
+// used internally
 typedef struct _joforth_word_details {
 
     // this is either a single entry or an array of entries terminated with the "kWordType_End" type
@@ -43,7 +52,7 @@ typedef struct _joforth_word_details {
     size_t                                  _depth;
 
 } _joforth_word_details_t;
-
+// used internally
 typedef struct _joforth_dict_entry {
     joforth_word_key_t              _key;
     const char*                     _word;
@@ -58,56 +67,67 @@ typedef struct _joforth_dict_entry {
 #define JOFORTH_DEFAULT_MEMORY_SIZE     0x8000
 #define JOFORTH_DEFAULT_RSTACK_SIZE     0x100
 
+// used internally
 typedef struct _joforth_rstack_entry {
     _joforth_dict_entry_t*      _entry;
     _joforth_word_details_t*    _details;
 } _joforth_rstack_entry_t;
 
+// used to provide allocator/free functionality, joForth doesn't use any other allocator
 typedef struct _joforth_allocator {
     void* (*_alloc)(size_t);
     void (*_free)(void*);
 } joforth_allocator_t;
 
+// the joForth VM state
 typedef struct _joforth {
     _joforth_dict_entry_t       *   _dict;
     joforth_value_t             *   _stack;
     _joforth_rstack_entry_t     *   _rstack;
-
-    joforth_allocator_t         *   _allocator;
-    
+    joforth_allocator_t         *   _allocator;    
     // current input base
     int                             _base;
-
     // if 0 then default, in units of joforth_value_t 
     size_t                          _stack_size;
-
     // if 0 then default, in units of _joforth_dict_entry_t 
-    size_t                          _rstack_size;
-    
+    size_t                          _rstack_size;    
     // stack pointers
     size_t                          _sp;
     size_t                          _rp;
     
 } joforth_t;
 
+// initialise the joForth VM
 void    joforth_initialise(joforth_t* joforth);
+// shutdown
 void    joforth_destroy(joforth_t* joforth);
-
+// add a word to the interpreter with an immediate evaluator (handler) and the required stack depth
 void    joforth_add_word(joforth_t* joforth, const char* word, joforth_word_handler_t handler, size_t depth);
-
+// evaluate a sequence of words (sentence)
+// for example:
+//  joforth_eval_word(&joforth, ": squared ( a -- a*a ) dup *  ;");
+//  joforth_eval_word(&joforth, "80");
+//  joforth_eval_word(&joforth, "squared");
+//
 void    joforth_eval_word(joforth_t* joforth, const char* word);
 
+// push a value on the stack (use this in your handlers)
 static _JOFORTH_ALWAYS_INLINE void    joforth_push_value(joforth_t* joforth, joforth_value_t value) {
     //TODO: assert on stack overflow
     joforth->_stack[joforth->_sp--] = value;
 }
+
+// pop a value off the stack (use this in your handlers)
 static _JOFORTH_ALWAYS_INLINE joforth_value_t joforth_pop_value(joforth_t* joforth) {
     //TODO: assert on stack underflow
     return joforth->_stack[++joforth->_sp];
 }
+// read top value from the stack (use this in your handlers)
 static _JOFORTH_ALWAYS_INLINE joforth_value_t    joforth_top_value(joforth_t* joforth) {
     return joforth->_stack[joforth->_sp+1];
 }
 
+// printf dictionary contents
 void    joforth_dump_dict(joforth_t* joforth);
+// dump current stack
 void    joforth_dump_stack(joforth_t* joforth);
